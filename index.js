14ce5f88027c2cbc0d8019f2ff826cbe40eb4c51
Ben Oluoch <splashmaxsoln@gmail.com>
2025-03-28 22:45:58 +0300

    Added index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(0.5 + 1);
+
+let num3 = 10
+let num4 = 6
+let mod = (num3 % num4)
+console.log(mod)
+
+const numbers = [4, 5, 9, 20, 7]; // Example set
+const max = Math.max(...numbers);    // Spread operator to expand the array
+
+console.log(max); // Output: 20 (since 20 is the highest number)
